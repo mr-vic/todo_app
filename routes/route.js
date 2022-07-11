99c251d9ba3e05e9869455d13f5e39cd6efb71b8
Viktor Tkachenko <mr.vic@bk.ru>
2022-07-11 16:20:37 +0500

    fix bug

--- routes/route.js
+++ routes/route.js
@@ -8,13 +8,12 @@ router.route('/task/create')
     .get(TaskController.create)
     .post(TaskController.createTask);
 
-router.route('/task/delete/:id')
-    .get(TaskController.delete)
-    .post(TaskController.deleteTask);
-
 router.route('/task/update/:id')
     .get(TaskController.update)
     .post(TaskController.updateTask);
 
+router.route('/task/delete/:id')
+    .get(TaskController.delete)
+    .post(TaskController.deleteTask);    
 
 module.exports = router;
